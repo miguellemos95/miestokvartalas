5d4f95ab8b4d86b4cb3413f9e15fa0aad3dba3bc
Miguel Lemos <1130720@isep.ipp.pt>
2020-11-12 18:59:11 +0000

    Auto-generated commit

---
+++ app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  @ViewChild('ngcarousel', { static: true }) ngCarousel: NgbCarousel;
+
+  ngOnInit() { }
+
+  // Move to specific slide
+  navigateToSlide(item) {
+    this.ngCarousel.select(item);
+    console.log(item)
+  }
+
+  // Move to previous slide
+  getToPrev() {
+    this.ngCarousel.prev();
+  }
+
+  // Move to next slide
+  goToNext() {
+    this.ngCarousel.next();
+  }
+
+  // Pause slide
+  stopCarousel() {
+    this.ngCarousel.pause();
+  }
+
+  // Restart carousel
+  restartCarousel() {
+    this.ngCarousel.cycle();
+  }
+}
